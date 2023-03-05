dce39dace5131513a8639e6079c68e64af73a404
lolsanti <55632755+lolsanti@users.noreply.github.com>
2023-03-06 01:41:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+###########################################
+
+#my_list = [98, 99, 100, 101, 102, 103]
+#for letter in my_list:
+    #if letter > 100:
+        #print(f"{letter} , больше 100 ")
+
+############################################
+
+#my_list = [98, 99, 100, 101, 102, 103]
+#my_result = []
+
+#for letter in my_list:
+    #if letter > 100:
+        #my_result.append(letter)
+        #print(my_result)
+
+#my_list = [4]
+#if len(my_list) < 2:
+    #my_list.append(0)
+#elif len(my_list) >= 2:
+    #print(my_list[-1] + my_list[-2])
